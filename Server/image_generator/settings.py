2f6021f990ec2eb43088a54f5a6c4f4690cb70d8
ForeverNoob69 <100515357+ForeverNoob69@users.noreply.github.com>
2023-04-19 18:02:48 +0600

    Update settings.py

--- Server/image_generator/settings.py
+++ Server/image_generator/settings.py
@@ -134,7 +134,7 @@ CORS_ALLOWED_ORIGINS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # MEDIA_URL = '/media/'
